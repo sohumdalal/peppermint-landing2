ee706bc77c29ac95ca811a1dae67b0284ff5d1d8
sohumdalal <dsohum@gmail.com>
2023-10-31 15:30:33 -0400

    landing page has links to all github and linkedin

--- src/components/MeetTeam.tsx
+++ src/components/MeetTeam.tsx
@@ -16,28 +16,31 @@ const MeetTheTeam = () => {
           <div className="xl:w-1/2 md:w-1/2 p-12">
             <div className="border border-gray-200 p-6 rounded-lg">
               <h2 className="text-lg text-white font-medium title-font mb-2">
-                Aliya Salmanov
+                Aliya Salmanova
               </h2>
-              <p className="leading-relaxed text-base text-white">
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod quisquam illum error quae tempora, nesciunt enim corrupti quia ipsum excepturi iste delectus accusantium voluptate quidem. Blanditiis animi labore consequuntur iste.
-              </p>
-              <a className="ml-3 text-white">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="0"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
+              <div className="sm:ml-auto sm:mt-0 mt-4 flex">
+                <a href="https://www.linkedin.com/in/aliya-salmanova-4150711b6/">
+                  <svg
+                    fill="white"
+                    className="w-5 h-5 mr-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+                <a href="https://github.com/AliyaSalmanova1">
+                  <svg
+                    fill="white"
+                    className="m1-3 w-5 h-5"
+                    viewBox="0 0 24 24">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
           <div className="xl:w-1/2 md:w-1/2 p-12">
@@ -45,26 +48,29 @@ const MeetTheTeam = () => {
               <h2 className="text-lg text-white font-medium title-font mb-2">
                 Edmund Wong
               </h2>
-              <p className="leading-relaxed text-base text-white">
-               Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio amet qui, veritatis consequuntur fugiat aspernatur ipsum id optio dignissimos tempore nulla modi. Corporis recusandae soluta aut nostrum inventore? Dolor, magnam!
-              </p>
-              <a className="ml-3 text-white">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="0"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
+              <div className="sm:ml-auto sm:mt-0 mt-4 flex">
+                <a href="https://www.linkedin.com/in/edmund-wong-8a0201291/">
+                  <svg
+                    fill="white"
+                    className="w-5 h-5 mr-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+                <a href="https://github.com/EmunW">
+                  <svg
+                    fill="white"
+                    className="m1-3 w-5 h-5"
+                    viewBox="0 0 24 24">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
           <div className="xl:w-1/2 md:w-1/2 p-12">
@@ -72,26 +78,29 @@ const MeetTheTeam = () => {
               <h2 className="text-lg text-white font-medium title-font mb-2">
                 Nassim Bahet
               </h2>
-              <p className="leading-relaxed text-base text-white">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore recusandae porro at odit nemo suscipit sapiente libero ut, exercitationem blanditiis praesentium neque! Facere ea, quam eaque vitae iusto dolorum minima?
-              </p>
-              <a className="ml-3 text-white">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="0"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
+              <div className="sm:ml-auto sm:mt-0 mt-4 flex">
+                <a href="https://www.linkedin.com/in/nassimbahet/">
+                  <svg
+                    fill="white"
+                    className="w-5 h-5 mr-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+                <a href="https://github.com/nassimBahet">
+                  <svg
+                    fill="white"
+                    className="m1-3 w-5 h-5"
+                    viewBox="0 0 24 24">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
           <div className="xl:w-1/2 md:w-1/2 p-12">
@@ -99,26 +108,29 @@ const MeetTheTeam = () => {
               <h2 className="text-lg text-white font-medium title-font mb-2">
                 Sohum Dalal
               </h2>
-              <p className="leading-relaxed text-base text-white">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis at assumenda sit enim beatae illum, perferendis architecto minus praesentium sunt! Quod doloremque ex aperiam amet eaque, atque impedit reiciendis maiores.
-              </p>
-                  <a className="ml-3 text-white">
-            <svg
-              fill="currentColor"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="0"
-              className="w-5 h-5"
-              viewBox="0 0 24 24"
-            >
-              <path
-                stroke="none"
-                d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
-              ></path>
-              <circle cx="4" cy="4" r="2" stroke="none"></circle>
-            </svg>
-          </a>
+              <div className="sm:ml-auto sm:mt-0 mt-4 flex">
+                <a href="https://www.linkedin.com/in/sohum-dalal-985076a6/">
+                  <svg
+                    fill="white"
+                    className="w-5 h-5 mr-3"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="none"
+                      d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                    ></path>
+                    <circle cx="4" cy="4" r="2" stroke="none"></circle>
+                  </svg>
+                </a>
+                <a href="https://github.com/sohumdalal">
+                  <svg
+                    fill="white"
+                    className="m1-3 w-5 h-5"
+                    viewBox="0 0 24 24">
+                    <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
+                  </svg>
+                </a>
+              </div>
             </div>
           </div>
         </div>
--- src/components/PeppermintFeatures.tsx
+++ src/components/PeppermintFeatures.tsx
@@ -16,7 +16,7 @@ const PeppermintFeatures = () => {
           </div>
         </div>
         <div className="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
-          <div className="p-4 md:w-1/3 flex flex-col text-center items-center "> 
+          <div className="p-4 md:w-1/3 flex flex-col text-center items-center ">
             <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-white text-blue-500 mb-5 flex-shrink-0">
               <svg
                 fill="none"
@@ -68,7 +68,7 @@ const PeppermintFeatures = () => {
               </p>
             </div>
           </div>
-          <div className="p-4 md:w-1/3 flex flex-col text-center items-center ">
+          <div className="p-4 md:w-1/3 flex flex-col text-center items-center">
             <div className="w-20 h-20 inline-flex items-center justify-center rounded-full bg-white mb-5 flex-shrink-0">
               <svg
                 fill="none"
