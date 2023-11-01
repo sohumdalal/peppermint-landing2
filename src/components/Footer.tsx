9c983f8d45ab9ed34e0acd4bdec8c6bfb504f912
sohumdalal <dsohum@gmail.com>
2023-11-01 10:59:38 -0400

    header/footer2

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="text-white body-font">
+      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+
+import { useScrollPosition } from "@/hooks/useScrollPosition";
+
+const Header = () => {
+  const scrollPosition = useScrollPosition();
+  return (
+    <header
+      className={`sticky top-0 z-50 transition-shadow ${scrollPosition > 0
+        ? "shadow bg-opacity-70 backdrop-blur-lg backdrop-filter"
+        : "shadow-none"
+        }`}
+    >
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center ">
+        <img src=""></img>
+        <a className="flex title-font font-medium items-center text-gray-100 mb-4 md:mb-0 ">
+          <h1 className="ml-3 text-4xl">PepperMint</h1>
+        </a>
+        <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+        </nav>
+        <a href="https://github.com/oslabs-beta/PepperMint">
+          <button className="inline-flex items-center bg-white text-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-500 hover:text-white rounded text-base mt-4 md:mt-0">
+            Github
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-4 h-4 ml-1"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
+
+
